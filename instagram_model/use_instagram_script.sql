3e1e7c119f6973d64059c65d288d5d336b8eba51
Ankita Mishra <78453293+midnightsun257@users.noreply.github.com>
2023-09-26 04:38:33 -0400

    use_instagram_script

---
+++ instagram_model/use_instagram_script.sql
@@ -0,0 +1,2 @@
+USE instagram_clone_db;
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
--- use_instagram_script.sql
+++
@@ -1,2 +0,0 @@
-USE instagram_clone_db;
-SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
